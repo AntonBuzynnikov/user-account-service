1a0b049de751156115ca8bf2aa63817faed78cb5
Anton Buzynnikov <a.buzynnikov@mail.ru>
2025-05-17 23:35:50 +0600

    Добавлены миграции

---
+++ src/main/java/ru/buzynnikov/user_acount_service/UserAcountServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.buzynnikov.user_acount_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAcountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAcountServiceApplication.class, args);
+	}
+
+}
